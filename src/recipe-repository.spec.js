89a02c7d94cf0fe393f6724ac40797c8832117fa
Younes JAAIDI <yjaaidi@gmail.com>
2019-04-15 15:26:25 +0200

    🗜 Added empty specs for `RecipeRepository`.

---
+++ src/recipe-repository.spec.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ *
+ * $Id: $
+ */
+
+'use strict';
+
+describe('RecipeRepository', () => {
+
+    it.skip('should add recipes', () => {
+        throw new Error('😱 Not implemented yet!');
+    });
+
+    it.skip('should remove recipes', () => {
+        throw new Error('😱 Not implemented yet!');
+    });
+
+    it.skip('should filter recipes', () => {
+        throw new Error('😱 Not implemented yet!');
+    });
+
+});
