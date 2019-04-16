db5f03148e8f4bd7382400d08205184409513364
Younes JAAIDI <yjaaidi@gmail.com>
2019-04-16 14:20:35 +0200

    🧹 Getting rid of `getDataWithCallback`.

--- src/index.ts
+++ src/index.ts
@@ -60,45 +60,24 @@ async function getDataWithPromise(): Promise<string> {
 
     const nodemon = JSON.parse(nodemonString) as NodemonConfig;
 
-    const indexPath = nodemon.exec.split(' ')[0];
+    const indexPath = nodemon.exec.split(' ')[1];
 
     return readFileAsString(indexPath);
 
 }
 
-function getDataWithCallback(callback: GetDataCallback) {
-
-    const nodemonFilePath = path.join(__dirname, '..', 'nodemon.json');
-
-    readFile(nodemonFilePath, (err, data) => {
-        if (err != null) {
-            callback(err, null);
-            return;
-        }
-
-        const nodemon: NodemonConfig = JSON.parse(data.toString());
-
-        const indexPath = nodemon.exec.split(' ')[1];
-
-        readFile(indexPath, (err, data) => {
-            if (err != null) {
-                callback(err, null);
-                return;
-            }
-
-            callback(null, data.toString());
-        });
-    });
-}
-
-const asyncHandler = (callback: (request: Request, response: Response)) => async (request: Request, response: Response) => {
+/**
+ * @todo use https://github.com/davidbanham/express-async-errors
+ * @param callback
+ */
+const asyncHandler = (callback: (request: Request, response: Response) => void) => async (request: Request, response: Response) => {
 
     try {
         await callback(request, response)
     }
     catch (err) {
         response.status(500);
-        response.send(err);
+        response.send(err.toString());
     }
 
 };
